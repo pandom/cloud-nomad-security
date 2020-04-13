fe5a750c6d37daa15e8fe3ed2d38474f5fff1da1
Anthony Burke <burkey@hashicorp.com>
2020-04-13 22:18:31 +1000

    modifies descriptions

--- acl-configuration/main.tf
+++ acl-configuration/main.tf
@@ -19,13 +19,13 @@ provider "nomad" {
 
 resource "nomad_acl_policy" "appdev" {
   name        = "appdev"
-  description = "Application Developer profile"
+  description = "Application Developer profile via Terraform"
   rules_hcl   = file("${path.module}/policies/app-dev.policy.hcl"
 }
 
 resource "nomad_acl_policy" "prodops" {
   name        = "prodops"
-  description = "Production Operations profile"
+  description = "Production Operations profile via Terraform"
   rules_hcl   = file("${path.module}/policies/prod-ops.policy.hcl"
 }
 
